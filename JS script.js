49eed09998c2691a81f6b6cefb11d14abab48ff2
Yudany Mora <ymoracanagua@gmail.com>
2020-12-11 10:02:36 -0500

    first commit

---
+++ JS script.js
@@ -0,0 +1,10 @@
+let nome = prompt('Digite seu nome:')
+let sobrenome = prompt('Digite seu Sobrenome:') 
+let idade = prompt('Digite sua idade:')
+let Nacionalidade = prompt('Digite sua Nacionalidade')
+
+alert ('Benvindo!');
+
+function enviarFormulario(){
+    console.log ('Enviando Formulario...');
+}
